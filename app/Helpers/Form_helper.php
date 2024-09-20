8698dc2cfff714818733d6a6dd7a410e32d6a1e0
amankass <137434910+amankass@users.noreply.github.com>
2024-09-20 02:24:23 -0800

    Update .gitignore to exclude unnecessary files

--- app/Helpers/Form_helper.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Displaying Errors to form after Validation
-function display_form_errors($validation, $field)
-{
-    if ($validation->hasError($field)) 
-    {
-        return $validation->getError($field);
-    }
-}
